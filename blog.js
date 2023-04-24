fc54a73e70b1a95403f2f57df8f99b0203718b00
Andres182358 <125666705+Andres182358@users.noreply.github.com>
2023-04-23 23:03:08 -0600

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,40 @@
+function iniciar(){
+    var boton=document.getElementById('guardar');
+    boton.addEventListener('click', nuevoitem, false);
+    mostrar();
+}
+function nuevoitem(){
+    var clave=document.getElementById('clave').value;
+    var valor=document.getElementById('texto').value;
+    localStorage.setItem(clave,valor);
+    mostrar();
+    document.getElementById('clave').value='';
+    document.getElementById('texto').value='';
+}
+function mostrar(){
+    var cajadatos=document.getElementById('cajadatos');
+    cajadatos.innerHTML='';
+    for(var f=0;f<localStorage.length;f++){
+    var clave=localStorage.key(f);
+    var valor=localStorage.getItem(clave);
+    cajadatos.innerHTML+='<div>'+clave+' - '+valor+'</div>';
+    }
+}
+    window.addEventListener('load', iniciar, false);
+function elimina()
+    {
+        if(confirm('Está Seguro?')){
+            localStorage.clear();
+            mostrar();
+            }
+    }
+    function elimina_d()
+    {
+        var clave=document.getElementById('clave').value;
+
+        if(confirm('Está Seguro?')){
+            localStorage.removeItem(clave);
+            document.getElementById('clave').value='';
+            mostrar();
+            }
+    }
