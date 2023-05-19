d83d24bfb079085568fc4f0b80cacedbd3c90884
Andres182358 <125666705+Andres182358@users.noreply.github.com>
2023-05-19 07:53:27 -0600

    Add files via upload

---
+++ Examen Parcial 3.js
@@ -0,0 +1,13 @@
+function enviar(){
+    if("fecha"=04){
+        <p>Este mes es tu cumpleaños!!</p>
+    }
+}
+function posicion(){
+    var nom=document.getElementById("fecha").value;
+    var resultado=nom("8");
+    document.getElementById("result").value=resultado;
+}
+if(posicion){
+    
+}
